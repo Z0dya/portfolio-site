f9d1d89f2066b724d79f81c333174058e677646a
Z0dya <vladislavlazutkin64@gmail.com>
2022-09-18 22:11:57 +0300

    popup new

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let datestart = document.querySelector("#date-start");
 let dateadvance = document.querySelector("#date-advance");
 let datepremium = document.querySelector("#date-premium");
 
-let burger = document.querySelector(".header__burger-btn");
+let popup = document.querySelector(".popup");
 
 tumbler.addEventListener("click", () => {
 	if (tumbler.checked) {
@@ -26,37 +26,25 @@ tumbler.addEventListener("click", () => {
 	}
 });
 
-// burger.addEventListener("click", () => {
-// 	document.querySelector(".popup").classList.toggle("hidden");
-// });
-
 const openMenu = document.querySelector(".header__burger-btn");
 
 openMenu.addEventListener("click", (e) => {
 	e.preventDefault();
-	openMenu.nextElementSibling.classList.toggle("hidden");
+	popup.classList.remove("hidden");
 });
 
-// $(".button").click(function () {
-// 	$(".hidden").toggle();
-// });
-// $(document).on("click", function (e) {
-// 	if (!$(e.target).closest("header__burger-btn").length) {
-// 		$(".popup").hide();
-// 	}
-// 	e.stopPropagation();
-// });
+document.addEventListener("click", (e) => {
+	if (!e.target.classList.contains("popup") && !e.target.classList.contains("header__burger-img")) {
+		popup.classList.add("hidden");
+	}
+});
 
-// document.addEventListener("click", function (e) {
-// 	let actMenu = document.querySelector(".popup.hidden");
-// 	if (actMenu && e.target !== actMenu && !actMenu.contains(e.target)) {
-// 		document.querySelector(".popup").classList.remove("hidden");
-// 	}
-// });
+
 
 //прокрутка страницы
 
 const anchors = document.querySelectorAll('a[href*="#"]');
+
 
 for (let anchor of anchors) {
 	anchor.addEventListener("click", function (e) {
